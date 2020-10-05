ccd2c7c79bf7d42ab11e16e2e99be5b6e23ae873
nefaden <durandyannetudes@gmail.com>
2020-10-05 17:45:29 +0200

    Initial commit

---
+++ Item.swift
@@ -0,0 +1,16 @@
+//
+//  Item.swift
+//  MyApp
+//
+//  Created by Yann Durand on 24/09/2020.
+//
+
+import Foundation
+
+class Item {
+   var name: String
+
+   init(name: String) {
+     self.name = name
+   }
+}
--- MyApp/ViewController.swift
+++ MyApp/ViewController.swift
@@ -8,12 +8,40 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBAction func cancel(sender: UIBarButtonItem) {
+       let isInAddMode = presentingViewController is UINavigationController
+        
+       if isInAddMode {
+         dismiss(animated: true, completion: nil)
+       }
+       else {
+         navigationController!.popViewController(animated: true)
+       }
+    }
+    
+    @IBAction func setLabelText(_ sender: UIButton) {
+        nameLabel.text = nameTextField.text
+    }
+    
+    var item: Item?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        if let item = item {
+           nameTextField.text = item.name
+        }
     }
-
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+      if sender as AnyObject? === saveButton {
+        let name = nameTextField.text ?? ""
+        item = Item(name: name)
+      }
+    }
+    
 }
 
